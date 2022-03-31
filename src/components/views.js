42b3a1881566c47b835082db95990704e2e7441a
sokkung9 <csy1506@gmail.com>
2022-03-31 23:08:08 +0900

    Add Fragments, Views, Blueprints#for import

---
+++ src/components/views.js
@@ -0,0 +1,8 @@
+export { default as VGuide         } from './views/v_guide';
+export { default as VLogin         } from './views/v_login';
+export { default as VMain          } from './views/v_main';
+export { default as VNotice        } from './views/v_notice';
+export { default as VPrivacyPolicy } from './views/v_privacy_policy';
+export { default as VQna           } from './views/v_qna';
+export { default as VRegister      } from './views/v_register';
+export { default as VTermsOfService} from './views/v_terms_of_service';
