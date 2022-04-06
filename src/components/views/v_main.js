ab40cce1d1a2d019d3f2e95c9a40588bf6056f5a
sokkung9 <csy1506@gmail.com>
2022-04-07 00:13:15 +0900

    Update VMain#add carousel views

--- src/components/views/v_main.js
+++ src/components/views/v_main.js
@@ -1,13 +1,121 @@
-import { MainContainer } from '../blueprints';
+import { CarouselCompletedCollabo, CarouselPopularMember, MainContainer } from '../blueprints';
 
 function VMain() {
+  var testPopularMembers = [
+    {
+      name: 'A',
+      sales: 100,
+      memberCategory: '제조1',
+      completedCollaboCount: 10,
+    },
+    {
+      name: 'B',
+      sales: 200,
+      memberCategory: '제조2',
+      completedCollaboCount: 20,
+    },
+    {
+      name: 'C',
+      sales: 300,
+      memberCategory: '제조3',
+      completedCollaboCount: 30,
+    },
+    {
+      name: 'D',
+      sales: 400,
+      memberCategory: '제조4',
+      completedCollaboCount: 40,
+    },
+    {
+      name: 'D',
+      sales: 500,
+      memberCategory: '제조4',
+      completedCollaboCount: 40,
+    },
+    {
+      name: 'D',
+      sales: 600,
+      memberCategory: '제조4',
+      completedCollaboCount: 40,
+    },
+    {
+      name: 'D',
+      sales: 700,
+      memberCategory: '제조4',
+      completedCollaboCount: 40,
+    },
+  ];
+
+  var testCompletedCollabos = [
+    {
+      title: 'title 1',
+      completedDate: '22.03.15',
+      member1: {
+        profileImg: 'image1',
+        memberName: 'SAMSUNG'
+      },
+      member2: {
+        profileImg: 'image2',
+        memberName: 'APPLE'
+      }
+    },
+    {
+      title: 'title 2',
+      completedDate: '22.03.16',
+      member1: {
+        profileImg: 'image1',
+        memberName: 'SAMSUNG'
+      },
+      member2: {
+        profileImg: 'image2',
+        memberName: 'APPLE'
+      }
+    },
+    {
+      title: 'title 3',
+      completedDate: '22.03.17',
+      member1: {
+        profileImg: 'image1',
+        memberName: 'SAMSUNG'
+      },
+      member2: {
+        profileImg: 'image2',
+        memberName: 'APPLE'
+      }
+    },
+    {
+      title: 'title 4',
+      completedDate: '22.03.18',
+      member1: {
+        profileImg: 'image1',
+        memberName: 'SAMSUNG'
+      },
+      member2: {
+        profileImg: 'image2',
+        memberName: 'APPLE'
+      }
+    },
+
+  ];
+
+  
 
   return (
     <div style={{minHeight: `${100}vh`}}>
-      <MainContainer className="w-100-full px-0 d-flex flex-column">
-        <div className="mg-t-100 d-flex flex-column justify-content-center align-items-center">
-          <div className="mg-t-20 fnt-size-10">로그인 후 서비스를 이용해 주세요! :)</div>
+      <MainContainer className="w-100-full d-flex flex-column">
+
+        <div className="bg-primary" style={{height: `${409}px`}}>
+          <MainContainer>
+            <div className="mg-t-50 color-ffffff fnt-size-12 fw-700">성사된 콜라보</div>
+            <CarouselCompletedCollabo completedCollabos={testCompletedCollabos}/>
+          </MainContainer>
         </div>
+
+        <MainContainer className="mg-t-165 mg-b-100">
+          <div className="fnt-size-12 fw-700" style={{color: "#6d6d6d"}}>인기업체</div>
+          <CarouselPopularMember popularMembers={testPopularMembers}/>
+        </MainContainer>
+        
       </MainContainer>
     </div>
   );
