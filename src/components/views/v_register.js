ae535e96120b108c341dcd88b318938b595518b6
sokkung9 <csy1506@gmail.com>
2022-04-12 21:15:31 +0900

    Update VRegister#add view after registration (WIP)

--- src/components/views/v_register.js
+++ src/components/views/v_register.js
@@ -1,30 +1,40 @@
 import React, { useState, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEyeSlash as fasEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { MainContainer } from '../blueprints';
 
 function VRegister() {
   var [isSubmit, setIsSubmit] = useState(false);
+  var [isRegistered, setIsRegistered] = useState(false);
   var [privacyChecked, setPrivacyChecked] = useState(false);
   var [serviceChecked, setServiceChecked] = useState(false);
+  var [overFourteen, setOverFourteen] = useState(false);
+  var [requestEmailVerification, setRequestEmailVerification] = useState(false);
   
-  var [userId, setUserId] = useState('');
+  var [email, setEmail] = useState('');
+  var [emailVerificationCode, setEmailVerificationCode] = useState('');
   var [password, setPassword] = useState('');
   var [password2, setPassword2] = useState('');
   var [companyName, setCompanyName] = useState('');
   var [coRegistNumber, setCoRegistNumber] = useState('');
-  var [idCheckMessage, setIdCheckMessage] = useState('');
-  var [pwCheckMessage, setPwCheckMessage] = useState('');
-  var [coRegistNumCheckMessage, setCoRegistNumCheckMessage] = useState('');
 
-  const idInputFocus = useRef(null);
+  var [emailCheckMsg, setEmailCheckMsg] = useState('');
+  var [emailVerificationCheckMsg, setEmailVerificationCheckMsg] = useState('');
+  var [pwCheckMsg, setPwCheckMsg] = useState('');
+  var [pw2CheckMsg, setPw2CheckMsg] = useState('');
+  var [coRegistNumCheckMsg, setCoRegistNumCheckMsg] = useState('');
+
+  const emailInputFocus = useRef(null);
+  const emailVerificationCodeInputFocus = useRef(null);
   const pwInputFocus = useRef(null);
   const pw2InputFocus = useRef(null);
   const companyNameInputFocus = useRef(null);
   const coRegistNumInputFocus = useRef(null);
   
   function validate() {
-    if (userId.trim()=== '') {
-      idInputFocus.current.focus();
-      return '아이디를 입력해 주세요.';
+    if (email.trim()=== '') {
+      emailInputFocus.current.focus();
+      return '이메일을 입력해 주세요.';
     }
     else if (password.trim() === '') {
       pwInputFocus.current.focus();
@@ -36,7 +46,7 @@ function VRegister() {
     }
     else if (password.trim() !== password2.trim()) {
       pwInputFocus.current.focus();
-      return '비밀번호와 비밀번호 확인이 다릅니다.';
+      return '비밀번호가 일치하지 않습니다.';
     }
     else if (companyName.trim() === '') {
       companyNameInputFocus.current.focus();
@@ -47,7 +57,7 @@ function VRegister() {
       return '사업자등록번호를 입력해 주세요.';
     }
     else if (!privacyChecked || !serviceChecked) {
-      return '이용약관 및 개인정보 수집이용 동의에 체크해 주세요.';
+      return '필수 동의에 체크해 주세요.';
     }
     else
       return true;
@@ -62,7 +72,9 @@ function VRegister() {
       setIsSubmit(true);
       try {
         // register 성공 시
-        alert('가입되었습니다.');
+        // alert('가입되었습니다.');
+        // window.location.href = '/';
+        setIsRegistered(true);
       } catch (e) {
         alert('register error: ', e);
         console.log('register error', e);
@@ -77,8 +89,10 @@ function VRegister() {
     const target = e.target;
     const name = target.name;
 
-    if (name === 'userId')
-      setUserId(target.value);
+    if (name === 'email')
+      setEmail(target.value);
+    else if (name === 'emailVerificationCode')
+      setEmailVerificationCode(target.value);
     else if (name === 'pw')
       setPassword(target.value);
     else if (name === 'pw2')
@@ -93,148 +107,229 @@ function VRegister() {
     if (!serviceChecked || !privacyChecked) {
       setServiceChecked(true);
       setPrivacyChecked(true);
+      setOverFourteen(true);
     } else {
       setServiceChecked(false);
       setPrivacyChecked(false);
+      setOverFourteen(false);
     }
   }
  
   function checkPwLength(e) {
-    // if (password.trim().length < 8)
-    //     setPwCheckMessage('비밀번호는 8자리 이상이어야 합니다.')
-    if (password.trim() !== password2.trim())
-      setPwCheckMessage('비밀번호와 비밀번호 확인이 다릅니다.')
-    else
-      setPwCheckMessage('')
+    const target = e.target;
+    const name = target.name;
+
+    if (name === 'pw') {
+      if (password.trim().length < 8)
+        setPwCheckMsg('영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.');
+      else 
+        setPwCheckMsg('');
+    }
+    
+    else if (name === 'pw2') {
+      if (password.trim() !== password2.trim())
+        setPw2CheckMsg('비밀번호가 일치하지 않습니다.');
+      else
+        setPw2CheckMsg('');
+    }
   }
 
 
   return (
-    <React.Fragment>
-      <MainContainer className="register">
-        {/* <div className=" flex-column justify-content-center align-items-center"> */}
-          <div className="my-5">
-            <div className="mg-t-50 fw-700 fnt-size-10">회원가입</div>
-            <div className="mg-t-40">
-              <div className="fw-700">아이디</div>
-              <div className="mg-t-10">
-                <input name="userId" type="text"
-                  className="py-2 px-3"
-                  style={{width: `${98}%`}}
-                  onChange={handleInputChange}
-                  placeholder="이메일을 입력해 주세요"
-                  autoFocus
-                  ref={idInputFocus}
-                />
-                {/* <div className="bg-primary color-ffffff fnt-size-6" style={{borderRadius: `${28}px`}}>인증하기</div> */}
-              </div>
-              <div className="mt-1 fnt-size-7">{idCheckMessage}</div>
+    <MainContainer className="w-100 bg-f0f0f0" style={{height: `${100}%`}}>
+    {isRegistered
+      ? /* 회원가입 성공 시 */
+        <div className="w-100 mx-auto pd-t-180 d-flex flex-column align-items-center justify-content-center">
+          <div className="pd-y-50 d-flex flex-column align-items-center justify-content-center" style={{width: `${686}px`, backgroundColor: "white"}}>
+            <div className="fnt-size-17 fw-700">회원가입이 완료되었습니다.</div>
+            <div className="text-center color-606060 fnt-size-10">
+              <div>반갑습니다. {companyName === '' ? '[회사명]' : companyName} 님</div>
+              <div>아직 프로필이 없으시네요! 프로필을 생성해주세요!</div>
             </div>
-
-            <div className="mg-t-20">
-              <div className="fw-700">비밀번호</div>
-              <div className="mg-t-10">
-                <input name="pw" type="password"
-                  className="py-2 px-3"
-                  style={{width: `${98}%`}}
-                  onChange={handleInputChange}
-                  // onKeyUp={checkPwLength}
-                  placeholder="비밀번호를 입력해주세요"
-                  ref={pwInputFocus}
-                />
-              </div>
-              <div className="mt-1">
-                <input name="pw2" type="password"
-                  className="py-2 px-3"
-                  style={{width: `${98}%`}}
-                  onChange={handleInputChange}
-                  onKeyUp={checkPwLength}
-                  placeholder="비밀번호를 입력해주세요"
-                  ref={pw2InputFocus}
-                />
-              </div>
-              <div className="mt-1 fnt-size-7 color-red">{pwCheckMessage}</div>
-            </div>
-
-            <div className="mg-t-20">
-              <div className="fw-700">회사명</div>
-              <div className="mg-t-10">
-                <input name="companyName" type="text"
-                  className="py-2 px-3"
-                  style={{width: `${98}%`}}
-                  onChange={handleInputChange}
-                  placeholder="회사명을 입력해주세요"
-                  ref={companyNameInputFocus}
-                />
-              </div>
-            </div>
-
-            <div className="mg-t-20">
-              <div className="fw-700">사업자등록번호</div>
-              <div className="mg-t-10">
-                <input name="coRegistNumber" type="text"
-                  className="py-2 px-3"
-                  style={{width: `${98}%`}}
-                  onChange={handleInputChange}
-                  placeholder="사업자등록번호를 입력해주세요"
-                  ref={coRegistNumInputFocus}
-                />
-              </div>
-              <div className="mt-1 fnt-size-7">{coRegistNumCheckMessage}</div>
-            </div>
-
-            <div className="" style={{bottom: `${38}%`, right: `${25}%`}}>
-              <button
-                type="button"
-                style={{borderRadius: `${28}px`}}
-                className={"w-100pct mg-t-40 py-3 btn fnt-size-9 fw-700" + (!isSubmit ? " btn-primary" : " btn-ccc")}
-                onClick={!isSubmit ? submitRegister : null}
-              >
-                {!isSubmit ? '가입하기' : '요청 중...'}</button>
-            </div>
-
-          <div className="mg-t-20 p-3" style={{border: "1px solid", borderRadius: `${10}px`}}>
+            <hr />
             <div className="d-flex">
               <div>
-                <input type="checkbox" name="all-terms" checked={serviceChecked && privacyChecked} onChange={handleAllCheckbox}/>
-                <label className="pd-l-6" htmlFor="agreementAllTerms">모두 동의합니다.</label>
+                <button type="button"
+                  style={{width: `${230}px`, height: `${60}px`}}
+                  className="btn btn-white-gray fnt-size-9 fw-500"
+                  onClick={(e) => window.location.href = "/"}
+                >
+                  나중에
+                </button>
               </div>
-            </div>
-            <hr className="mb-0 mg-t-10" style={{height: `${1}px`, opacity: 1}}/>
-            <div className="py-1 d-flex justify-content-between">
-              <div>
-                <input type="checkbox" name="service-terms" checked={serviceChecked} onChange={e => setServiceChecked(!serviceChecked)}/>
-                <label className="pd-l-6" htmlFor="agreementServiceTerms">서비스 이용약관에 동의합니다.</label>
+              <div className="mg-l-10">
+                <button type="button"
+                  style={{width: `${230}px`, height: `${60}px`}}
+                  className="btn btn-primary fnt-size-9 fw-500"
+                  // 프로필 작성으로 이동
+                  // onClick={(e) => window.location.href = "/"}
+                >
+                  작성하기
+                </button>
               </div>
-              <button
-                className="btn py-0 fnt-size-6"
-                onClick={(e) => {
-                  e.preventDefault();
-                  window.open("/docs/terms-of-service", "이용약관")
-                }}
-              >
-                약관 보기
-              </button>
-            </div>
-            <div className="py-1 d-flex justify-content-between">
-              <div>
-                <input type="checkbox" name="privacy-terms" checked={privacyChecked} onChange={e => setPrivacyChecked(!privacyChecked)}/>
-                <label className="pd-l-6" htmlFor="agreementPrivacyTerms">개인정보 수집/이용에 동의합니다.</label>
-              </div>
-              <button
-                className="btn py-0 fnt-size-6"
-                onClick={(e) => {
-                  e.preventDefault();
-                  window.open("/docs/privacy-policy", "개인정보처리및방침")
-                }}
-              >
-                개인정보처리방침
-              </button>
             </div>
           </div>
+          <div className="pd-b-180"></div>
         </div>
-      </MainContainer>
-    </React.Fragment>
+      :
+        <div>
+          <div className="pd-t-100 text-center fw-700 fnt-size-17">회원가입</div>
+          <div className="w-100 mx-auto bg-f0f0f0" style={{maxWidth: `${476}px`}}>
+            <div className="register mg-t-30">
+              <div className="pd-50 flex-wrap">
+                <div className="flex-shrink-0 fnt-size-8">이메일</div>
+                <div className="mg-t-6 d-flex align-items-center">
+                  <div className="flex-fill">
+                    <input name="email" type="text"
+                      className="fnt-size-8"
+                      style={{width: `${100}%`}}
+                      onChange={handleInputChange}
+                      placeholder="예:collabiz@sideproject.com"
+                      autoFocus
+                      ref={emailInputFocus}
+                    />
+                  </div>
+                  <div className="flex-shrink-0">
+                    <button className="btn btn-outline2-primary fw-500"
+                      style={{width: `${96}px`, height: `${54}px`, marginLeft: `${10}px`}}
+                    >
+                      인증번호</button>
+                  </div>
+                </div>
+                <div className="mt-1 fnt-size-7">{emailCheckMsg}</div>
+                <div className="mg-t-6 d-flex align-items-center">
+                  <div className="flex-fill">
+                    <input name="emailVerificationCode" type="text"
+                      className="fnt-size-8"
+                      style={{width: `${100}%`}}
+                      onChange={handleInputChange}
+                      placeholder="인증번호 6자리를 입력해주세요."
+                      ref={emailVerificationCodeInputFocus}
+                    />
+                  </div>
+                  <div className="flex-shrink-0">
+                    <button className="btn btn-outline2-primary fw-500"
+                      style={{width: `${96}px`, height: `${54}px`, marginLeft: `${10}px`}}
+                    >
+                      확인</button>
+                  </div>
+                </div>
+                <div className="mt-1 fnt-size-7">{emailVerificationCheckMsg}</div>
+
+
+                <div className="mg-t-20">
+                  <div className="flex-shrink-0 fnt-size-8">비밀번호</div>
+                  <div className="mg-t-6">
+                    <input name="pw" type="password"
+                      className="fnt-size-8"
+                      style={{width: `${100}%`}}
+                      onChange={handleInputChange}
+                      onKeyUp={checkPwLength}
+                      placeholder="비밀번호를 입력해주세요."
+                      ref={pwInputFocus}
+                    />
+                  </div>
+                  <div className="mt-1 fnt-size-7 color-red">{pwCheckMsg}</div>
+                </div>
+                
+                
+                <div className="mg-t-10">
+                  <div className="input-icon-container d-flex flex-row align-items-center overflow-hidden"
+                    // style={{border: "solid 1px #dedede"}}
+                  >
+                    <input name="pw2" type="password"
+                      className="fnt-size-8"
+                      // style={{padding: `${0.5}em`, flex: "auto 1 1", width: `${100}%`, border: "none"}}
+                      onChange={handleInputChange}
+                      onKeyUp={checkPwLength}
+                      placeholder="비밀번호를 한번 더 입력해주세요."
+                      ref={pw2InputFocus}
+                    />
+                    <FontAwesomeIcon icon={fasEyeSlash} className="color-dedede" style={{padding: `${14}px`,}}/>
+                  </div>
+                  <div className="mt-1 fnt-size-7 color-red">{pw2CheckMsg}</div>
+                </div>
+
+                <div className="mg-t-20">
+                  <div className="flex-shrink-0 fnt-size-8">회사명</div>
+                  <div className="mg-t-6">
+                    <input name="companyName" type="text"
+                      className="fnt-size-8"
+                      style={{width: `${100}%`}}
+                      onChange={handleInputChange}
+                      placeholder="회사명을 입력해주세요."
+                      ref={companyNameInputFocus}
+                    />
+                  </div>
+                </div>
+
+                <div className="mg-t-20">
+                  <div className="flex-shrink-0 fnt-size-8">사업자등록번호</div>
+                  <div className="mg-t-6">
+                    <input name="coRegistNumber" type="text"
+                      className="fnt-size-8"
+                      style={{width: `${100}%`}}
+                      onChange={handleInputChange}
+                      placeholder="사업자등록번호를 입력해주세요."
+                      ref={coRegistNumInputFocus}
+                    />
+                  </div>
+                  <div className="mt-1 fnt-size-7">{coRegistNumCheckMsg}</div>
+                </div>
+
+
+                <div className="mg-t-40">
+                  <div className="custom-checkbox">
+                    <input className="flex-shrink-0" type="checkbox" name="all-terms" checked={serviceChecked && privacyChecked && overFourteen} onChange={handleAllCheckbox}/>
+                    <label className="pd-l-12 fnt-size-10 fw-700" htmlFor="agreementAllTerms">모두 동의합니다.</label>
+                  </div>
+                  <hr style={{height: `${1}px`, opacity: 1}}/>
+                  <div>
+                    <input className="flex-shrink-0" type="checkbox" name="over-fourteen" checked={overFourteen} onChange={e => setOverFourteen(!overFourteen)}/>
+                    <label className="pd-l-12 fnt-size-8" htmlFor="overFourteen">만 14세 이상입니다. <span className="color-red">(필수)</span></label>
+                  </div>
+                  <div className="mg-t-10">
+                    <input className="flex-shrink-0" type="checkbox" name="service-terms" checked={serviceChecked} onChange={e => setServiceChecked(!serviceChecked)}/>
+                    <label className="pd-l-12 fnt-size-8" htmlFor="agreementServiceTerms">
+                      <button className="px-0 nt-size-9 color-primary btn-none"
+                        // style={{textDecoration: "underline"}}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          window.open("/docs/terms-of-service", "이용약관")
+                        }}
+                      >
+                        서비스 이용약관</button>에 동의합니다. <span className="color-red">(필수)</span></label>
+                  </div>
+                  <div className="mg-t-10 align-items-center">
+                    <input className="flex-shrink-0" type="checkbox" name="privacy-terms" checked={privacyChecked} onChange={e => setPrivacyChecked(!privacyChecked)}/>
+                    <label className="pd-l-12 fnt-size-8" htmlFor="agreementPrivacyTerms">
+                    <button className="px-0 fnt-size-8 color-primary btn-none"
+                      // style={{textDecoration: "underline"}}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        window.open("/docs/privacy-policy", "개인정보처리및방침")
+                      }}
+                    >
+                      개인정보 수집/이용</button>에 동의합니다. <span className="color-red">(필수)</span></label>
+                  </div>
+                </div>
+
+                <div className="" style={{bottom: `${38}%`, right: `${25}%`}}>
+                  <button
+                    type="button"
+                    style={{borderRadius: `${3}px`, backgroundColor: "#691B9A"}}
+                    className={"w-100pct mg-t-30 py-3 btn fnt-size-9 fw-700" + (!isSubmit ? " btn-primary" : " btn-ccc")}
+                    onClick={!isSubmit ? submitRegister : null}
+                  >
+                    {!isSubmit ? '가입하기' : '요청 중...'}</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="pd-b-100"></div>
+        </div>
+      }
+    </MainContainer>
   );
 }
 
