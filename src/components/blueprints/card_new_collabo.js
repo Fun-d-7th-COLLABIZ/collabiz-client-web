7c49df3541e036cf135f95763b2b44cc7ad56f9d
soyeong9 <csy1506@gmail.com>
2022-05-01 16:12:52 +0900

    Update VMain, CardNewCollabo

--- src/components/blueprints/card_new_collabo.js
+++ src/components/blueprints/card_new_collabo.js
@@ -1,6 +1,57 @@
-function CardNewCollabo() {
+function CardNewCollabo(props) {
   return (
-    <div>CardNewCollabo</div>
+    <div className="pd-x-30 pd-y-30 mg-t-25"
+      style={Object.assign({backgroundColor: "white", width: "380px !important", height: "296px", borderRadius: "8px", flex: "0 0 31.7%"}, ((props.idx + 1) % 3 === 0) ? {} : {marginRight: "16px"})}>
+      <div className="d-flex">
+        <img className="rounded-circle size-60" alt="new_collabo_member" src={`${process.env.PUBLIC_URL}/images/test_image.png`}/>
+        <div className="d-flex justify-content-between align-items-center">
+          <div className="flex-column mg-l-20">
+            <div className="fw-700" style={{fontSize: "15px", color: "#6D6D6D"}}>{props.newCollabo.companyName}</div>
+            <div className="color-838383 fnt-size-5">{props.newCollabo.location}</div>
+          </div>
+          <div className="d-flex">
+            <div className="flex-column">
+              <img className="" alt="heart_grey"
+                src={`${process.env.PUBLIC_URL}/images/heart_grey.png`}
+                style={{width: "20px", height: "18px"}}
+              />
+              <div className="text-center color-838383 fnt-size-5">{props.newCollabo.likeCount}</div>
+            </div>
+            <div className="flex-column mg-l-14">
+              <img className="" alt="view"
+                src={`${process.env.PUBLIC_URL}/images/view.png`}
+                style={{width: "28px", height: "18px"}}
+              />
+              <div className="text-center color-838383 fnt-size-5">{props.newCollabo.viewCount}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-3 color-primary fw-700" style={{fontSize: "17px"}}>{props.newCollabo.title}</div>
+      <div className="mt-3 d-flex justify-content-center flex-wrap">
+        {props.newCollabo.tags.map((v, i, a) => (
+          <div key={i}
+            className="pd-x-10 me-2 flex-shrink-0 fnt-size-5 fw-500"
+            style={{color: "#939393", wordBreak: "break-all", maxWidth: `${100}%`, border: "1px solid #939393", borderRadius: `${15}px`}}
+          >
+            {`# ${v}`}
+          </div>
+        ))}
+      </div>
+      <div className="mt-4 color-838383 fnt-size-6">
+        {props.newCollabo.content}
+      </div>
+      <div className="mt-4 d-flex justify-content-between align-items-center">
+        <div>
+          <button className="btn py-1 px-3 btn-primary fw-500"
+            style={{fontSize: "13px", borderRadius: "5px"}}
+          >
+            상세보기</button>
+        </div>
+        <div className="color-838383 fnt-size-5 fw-500">모집 마감일: {props.newCollabo.endDate}</div>
+      </div>
+    </div>
   );
 }
 
--- src/components/views/v_main.js
+++ src/components/views/v_main.js
@@ -1,6 +1,78 @@
-import { CarouselCompletedCollabo, CarouselPopularMember, MainContainer } from '../blueprints';
-
+import { CardNewCollabo, CarouselCompletedCollabo, CarouselPopularMember, MainContainer, MainSearchAndCollaboBox } from '../blueprints';
 function VMain() {
+  var testNewCollabos = [
+    {
+      companyName: 'A',
+      location: '서울 강남',
+      likeCount: 59,
+      viewCount: 10,
+      title: '개발인력 콜라보 원합니다.',
+      tags: ['프론트엔드', '개발자', '웹기획', '파이썬'],
+      content: '저희는 현재 이러이러한  회사입니다. 현재 프로젝트를 개발하기 위해서 프론트엔드 개발을 같이 할 기업이 있을까요?',
+      endDate: '22.03.15'
+    },
+    {
+      companyName: 'B',
+      location: '서울 강남',
+      likeCount: 69,
+      viewCount: 20,
+      title: '개발인력 콜라보 원합니다.',
+      tags: ['프론트엔드', '개발자', '웹기획', '파이썬'],
+      content: '저희는 현재 이러이러한  회사입니다. 현재 프로젝트를 개발하기 위해서 프론트엔드 개발을 같이 할 기업이 있을까요?',
+      endDate: '22.03.15'
+    },
+    {
+      companyName: 'C',
+      location: '서울 강남',
+      likeCount: 79,
+      viewCount: 30,
+      title: '개발인력 콜라보 원합니다.',
+      tags: ['프론트엔드', '개발자', '웹기획', '파이썬'],
+      content: '저희는 현재 이러이러한  회사입니다. 현재 프로젝트를 개발하기 위해서 프론트엔드 개발을 같이 할 기업이 있을까요?',
+      endDate: '22.03.15'
+    },
+    {
+      companyName: 'D',
+      location: '서울 강남',
+      likeCount: 89,
+      viewCount: 40,
+      title: '개발인력 콜라보 원합니다.',
+      tags: ['프론트엔드', '개발자', '웹기획', '파이썬'],
+      content: '저희는 현재 이러이러한  회사입니다. 현재 프로젝트를 개발하기 위해서 프론트엔드 개발을 같이 할 기업이 있을까요?',
+      endDate: '22.03.15'
+    },
+    {
+      companyName: 'D',
+      location: '서울 강남',
+      likeCount: 99,
+      viewCount: 40,
+      title: '개발인력 콜라보 원합니다.',
+      tags: ['프론트엔드', '개발자', '웹기획', '파이썬'],
+      content: '저희는 현재 이러이러한  회사입니다. 현재 프로젝트를 개발하기 위해서 프론트엔드 개발을 같이 할 기업이 있을까요?',
+      endDate: '22.03.15'
+    },
+    {
+      companyName: 'D',
+      location: '서울 강남',
+      likeCount: 109,
+      viewCount: 40,
+      title: '개발인력 콜라보 원합니다.',
+      tags: ['프론트엔드', '개발자', '웹기획', '파이썬'],
+      content: '저희는 현재 이러이러한  회사입니다. 현재 프로젝트를 개발하기 위해서 프론트엔드 개발을 같이 할 기업이 있을까요?',
+      endDate: '22.03.15'
+    },
+    {
+      companyName: 'D',
+      location: '서울 강남',
+      likeCount: 119,
+      viewCount: 40,
+      title: '개발인력 콜라보 원합니다.',
+      tags: ['프론트엔드', '개발자', '웹기획', '파이썬'],
+      content: '저희는 현재 이러이러한  회사입니다. 현재 프로젝트를 개발하기 위해서 프론트엔드 개발을 같이 할 기업이 있을까요?',
+      endDate: '22.03.15'
+    },
+  ];
+
   var testPopularMembers = [
     {
       name: 'A',
@@ -99,6 +171,11 @@ function VMain() {
   ];
 
   
+  const _limit = 6;
+  var newCollaboCards = [];
+  for (var i = 0; i < _limit; i++) {
+    newCollaboCards.push(<CardNewCollabo key={i} idx={i} newCollabo={testNewCollabos[i]}/>);
+  }
 
   return (
     <div className="w-100-full" style={{minHeight: `${100}vh`}}>
@@ -107,6 +184,22 @@ function VMain() {
           <img src={`${process.env.PUBLIC_URL}/images/main_background_top.png`} alt="main_background_top"
             style={{backgroundColor: "#704C79", backgroundBlendMode: "multiply"}}
           />
+        </div>
+        <div className="pd-t-40" style={{height: `${793}px`, backgroundColor: "#F7F5F7"}}>
+          <MainContainer>
+            <div className="d-flex justify-content-between align-item-center">
+              <div className="fnt-size-12 fw-700" style={{color: "#6D6D6D"}}>신규 콜라보</div>
+              <div className="" style={{border: "1px solid #8D7A92", borderRadius: "26px"}}>
+                <button className="btn py-1 px-4 fnt-size-8 fw-700" style={{color: "#562C62"}}>더보기</button>
+              </div>
+            </div>
+            <div className="d-flex flex-wrap">
+              {/* {testNewCollabos.map((c, i) => 
+                <CardNewCollabo key={i} idx={i} newCollabo={c}/>
+              )} */}
+              {newCollaboCards}
+            </div>
+          </MainContainer>
         </div>
 
         <div style={{height: `${409}px`, backgroundImage: "url(/images/main_background_mid.png)", backgroundColor: "#704C79"}}>
