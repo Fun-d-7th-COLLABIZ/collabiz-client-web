7d0cb595f06bb12d87fdb8611cb23140bf388765
soyeong9 <csy1506@gmail.com>
2022-05-30 23:29:41 +0900

    Update LeftSidebarGuide

--- src/components/fragments/left_sidebar_guide.js
+++ src/components/fragments/left_sidebar_guide.js
@@ -4,8 +4,8 @@ function LeftSidebarGuide() {
   const location = useLocation();
 
   return (
-    <div className="mg-y-30 py-5 px-2"
-      style={{borderRadius: `${20}px`, backgroundColor: "#F7F7F7", width: `${198}px`, height: `${685}px`}}
+    <div className="bg-ffffff pd-t-100 ps-4"
+      style={{width: `${230}px`, height: `${685}px`}}
     >
       <nav className= "flex-shrink-0 d-flex flex-column"
         style={{width: `${198}px`}}
@@ -15,14 +15,14 @@ function LeftSidebarGuide() {
           {link: '/guide'                , name: '이용방법'       },
           {link: '/notice'               , name: '공지사항'       },
           {link: '/docs/terms-of-service', name: '이용약관'       },
-          {link: '/docs/privacy-policy'  , name: '개인정보처리및방침'},
+          {link: '/docs/privacy-policy'  , name: '개인정보처리방침' },
           {link: '/qna'                  , name: '문의하기'       },
         ].map((v, i) => {
           return (
             <NavLink key={i}
               to={{pathname: v.link}}
               state={{scrollTo: [0, 0]}}
-              className={"nav-link" + (v.link === location.pathname ? " fw-600"  : "")}
+              className={"nav-link py-3" + (v.link === location.pathname ? " fw-700 color-primary"  : " color-979797")}
             >
               · {v.name}
             </NavLink>
